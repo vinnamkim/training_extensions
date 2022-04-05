7846bf7924173bcac4cb90d68fb60be3a3021e72
Kim, Vinnam <vinnam.kim@intel.com>
2022-04-05 15:13:16 +0900

    Update
    Signed-off-by: Kim, Vinnam <vinnam.kim@intel.com>

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
-docker run --gpus all -d -p 8888:8888 \
+nvidia-docker run -d -p 8888:8888 \
     --name=ote-jupyter \
     --ipc=host \
-    -v /local/dataset:/dataset \
-    -v /home/vinnamki/outputs:/outputs \
+    -v /local_ssd3/dataset:/dataset \
+    -v /local_ssd3/vinnamki/outputs:/outputs \
     -v torch-cache:/root/.cache/torch \
     -v torch-model:/root/.torch/models \
     otedet-jupyter:v0.1.1
